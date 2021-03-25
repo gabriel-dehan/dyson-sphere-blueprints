e1db2076c664573185232a6f24aefc2ffb6c42f0
Gabriel Dehan <dehan.gabriel@gmail.com>
2021-03-25 22:52:15 +0100

    WIP Renderer using external package

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "stimulus"
-import Preview3DRenderer from '../blueprintEditor/3D/renderer';
+import Preview3DRenderer from 'brokenmass3dpreview';
 
 export default class extends Controller {
   static targets = [ "data", "output", "tooltip" ]
@@ -19,7 +19,10 @@ export default class extends Controller {
         return `<p>originalId  ${data.originalId}</p>
          <p>modelIndex: ${data.modelIndex}</p>
          <p>recipeId:   ${data.recipeId}</p>`;
-      }
+      },
+      assetPathResolver: (assetType, id) => {
+        return `https://dyson-sphere-blueprints-dev.s3-eu-west-1.amazonaws.com/public/game_icons/${assetType}/${id}.png`;
+      },
     });
 
     renderer.on('render:start', function() {
