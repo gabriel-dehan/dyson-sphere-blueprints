3fea475fe1b1abe447cf180199b5a9b46dd605a5
Gabriel Dehan <dehan.gabriel@gmail.com>
2021-03-26 21:47:36 +0100

    Added tab for rendering preview

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -11,59 +11,61 @@ const assetPathResolver = (assetType, id) => {
   }
 }
 export default class extends Controller {
-  static targets = [ "data", "output", "tooltip" ]
+  static targets = [ "data", "output", "tooltip", "loader" ]
 
-  connect() {
+  render() {
     const tooltipContainer = this.tooltipTarget;
     const container = this.outputTarget;
     const data = this.dataTarget.value;
 
-    const renderer = new Preview3DRenderer({
-      tooltipContainer,
-      container,
-      data,
-      setTooltipContent: (data) => {
-        const { protoId, recipeId } = data;
-        const entityHtml = `
-          <span class="o-preview-tooltip__entity">
-            <img src="${assetPathResolver('entities', protoId)}" />
-            <h4>${Entities[protoId]}</h4>
-          </span>`;
-
-
-        let recipeHtml = '';
-
-        if (recipeId && recipeId > 0) {
-          recipeHtml = `
-            <span class="o-preview-tooltip__recipe">
-              <img src="${assetPathResolver('recipes', recipeId === 0 ? 'default' : recipeId )}" />
-              <h4>Recipe: ${Recipes[recipeId]}</h4>
+    // Only render if output is empty (hasn't been rendered yet)
+    if (!container.childNodes.length) {
+      const renderer = new Preview3DRenderer({
+        tooltipContainer,
+        container,
+        data,
+        setTooltipContent: (data) => {
+          const { protoId, recipeId } = data;
+          const entityHtml = `
+            <span class="o-preview-tooltip__entity">
+              <img src="${assetPathResolver('entities', protoId)}" />
+              <h4>${Entities[protoId]}</h4>
             </span>`;
-        }
-
-        return `
-          <span class="o-preview-tooltip__content">
-            ${entityHtml}
-            ${recipeHtml}
-          </span>
-        `;
-      },
-      assetPathResolver,
-    });
-
-    renderer.on('render:start', function() {
-      // TODO: Loader
-    })
-
-    renderer.on('render:complete', function() {
-      // TODO: Loader
-    })
-
-    renderer.on('entity:select', function(data) {
-      // console.log('Select', data);
-    })
 
 
-    renderer.render();
+          let recipeHtml = '';
+
+          if (recipeId && recipeId > 0) {
+            recipeHtml = `
+              <span class="o-preview-tooltip__recipe">
+                <img src="${assetPathResolver('recipes', recipeId === 0 ? 'default' : recipeId )}" />
+                <h4>Recipe: ${Recipes[recipeId]}</h4>
+              </span>`;
+          }
+
+          return `
+            <span class="o-preview-tooltip__content">
+              ${entityHtml}
+              ${recipeHtml}
+            </span>
+          `;
+        },
+        assetPathResolver,
+      });
+
+      renderer.on('render:start', () => {
+        this.loaderTarget.classList.remove('hidden');
+      })
+
+      renderer.on('render:complete', () => {
+        this.loaderTarget.classList.add('hidden');
+      })
+
+      renderer.on('entity:select', (data) => {
+        // console.log('Select', data);
+      })
+
+      renderer.render();
+    }
   }
 }
--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -3,20 +3,29 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "select", "tab" ]
 
-  connect() {
+  select(event) {
     const selectors = this.selectTarget.querySelectorAll('[data-tab-select]');
     const tabs = this.tabTargets;
+    const targetId = event.target.dataset.tabSelect;
+    const selector = this.selectTarget.querySelector(`[data-tab-select="${targetId}"]`);
+    const target = tabs.find(tab => tab.dataset['tabsId'] == targetId);
 
-    selectors.forEach((selector) => {
-      selector.addEventListener('click', () => {
-        const targetId = selector.dataset['tabSelect'];
-        const target = tabs.find(tab => tab.dataset['tabsId'] == targetId);
-        console.log(tabs, targetId, target)
-        selectors.forEach(sel => sel.classList.remove('active'))
-        tabs.forEach(tab => tab.classList.remove('active'))
-        selector.classList.add('active');
-        target.classList.add('active');
-      })
-    })
+    // Reset everything
+    selectors.forEach(sel => sel.classList.remove('active'))
+    tabs.forEach(tab => tab.classList.remove('active'))
+
+    // Add active where needed
+    selector.classList.add('active');
+    target.classList.add('active');
+
+    if (targetId === '3d-preview') {
+      // Defer the execution just a tiny bit to give the tab change a chance
+      setTimeout(() => this.previewController.render(), 500)
+
+    }
+  }
+
+  get previewController() {
+    return this.application.getControllerForElementAndIdentifier(document.querySelector("[data-controller*='preview']"), "preview")
   }
 }
