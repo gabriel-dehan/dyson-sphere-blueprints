67761ac7a250e08b67b3e3eb3989fb9a426005da
Gabriel Dehan <dehan.gabriel@gmail.com>
2021-08-24 20:06:43 +0200

    Default mailer from name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "dysonsphereblueprints@gmail.com"
+  default from: "Dyson Sphere Blueprints <dysonsphereblueprints@gmail.com>"
   layout "mailer"
 end
