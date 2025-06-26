54ea40439b4b76117c830fbc5c0ad5d6b6138acc
Markwart von Bonin <markwartbonin@hotmail.com>
2025-06-26 14:14:35 +0200

    added arm64-darwin-24 to gemfile, Moved comment db migrations to one file.

--- db/migrate/20250521110311_create_comments.rb
+++ db/migrate/20250521110311_create_comments.rb
@@ -4,8 +4,13 @@ class CreateComments < ActiveRecord::Migration[6.1]
       t.text :content
       t.references :blueprint, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.integer :parent_id
+      t.datetime :deleted_at
 
       t.timestamps
     end
+
+    add_index :comments, :parent_id
+    add_foreign_key :comments, :comments, column: :parent_id, on_delete: :cascade
   end
 end
--- db/migrate/20250521131232_add_deleted_at_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddDeletedAtToComments < ActiveRecord::Migration[6.1]
-  def change
-    add_column :comments, :deleted_at, :datetime
-  end
-end
