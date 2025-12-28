9db325592596975ddbedac9851b0bac110daa4c4
Gabriel Dehan <dehan.gabriel@gmail.com>
2025-12-28 03:24:50 +0100

    Update to latest version of dsp parser

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,4 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-
-# Add lib/ to load path for Ruby 3.2+ compatibility workarounds
-$LOAD_PATH.unshift(File.expand_path("../lib", __dir__)) unless $LOAD_PATH.include?(File.expand_path("../lib", __dir__))
-require "logger" # Required for Ruby 3.2+ compatibility with Rails 6.1
-require "digest/md5" # Required for dsp_blueprint_parser compatibility with Ruby 3.2+
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
--- lib/md5f.rb
+++
@@ -1,3 +0,0 @@
-# Workaround for dsp_blueprint_parser bug with Ruby 3.2+
-# The gem incorrectly requires 'md5f' instead of 'digest/md5'
-require 'digest/md5'
