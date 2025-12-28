d7fb61efc4a057863a33bbe9977849ffec747de2
Gabriel Dehan <dehan.gabriel@gmail.com>
2025-12-28 23:30:03 +0100

    Add CI workflow

--- Gemfile
+++ Gemfile
@@ -96,9 +96,8 @@ end
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem "capybara", ">= 3.26"
-  gem "selenium-webdriver"
-  # Easy installation and use of web drivers to run system tests with browsers
-  gem "webdrivers"
+  # Selenium 4.11+ includes Selenium Manager for automatic chromedriver management
+  gem "selenium-webdriver", ">= 4.11"
 end
 
 group :staging, :production do
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,12 +1,7 @@
 require "test_helper"
 
-# Disable webdrivers auto-update to avoid SSL errors
-# System tests require chromedriver to be manually installed
-if defined?(Webdrivers::Chromedriver)
-  Webdrivers::Chromedriver.required_version = ENV.fetch("CHROMEDRIVER_VERSION", nil)
-end
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # Selenium Manager (built into selenium-webdriver 4.11+) handles chromedriver automatically
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
